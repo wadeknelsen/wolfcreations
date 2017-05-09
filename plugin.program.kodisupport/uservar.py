6fc118ec3cb8fd4ca98912dd83ca6b2c1d0a684a
wadeknelsen <wadeknelsen@gmail.com>
2017-05-09 06:09:43 -0600

    wizard fix

--- plugin.program.kodisupport/uservar.py
+++ plugin.program.kodisupport/uservar.py
@@ -7,7 +7,7 @@ ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
 ADDONTITLE     = 'Kodisupport Wizard'
 EXCLUDES       = [ADDON_ID]
 # Text File with build info in it.
-BUILDFILE      = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/buildssfile.txt'
+BUILDFILE      = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/buildsfile.txt'
 # How often you would list it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK    = 0

5f714c8e53f1396b695a2929bbd9657e8429c91d
wadeknelsen <wadeknelsen@gmail.com>
2017-05-11 12:26:31 -0600

    new stuff

--- plugin.program.kodisupport/uservar.py
+++ plugin.program.kodisupport/uservar.py
@@ -4,22 +4,22 @@ import os, xbmc, xbmcaddon
 ### User Edit Variables #################################
 #########################################################
 ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
-ADDONTITLE     = 'Kodisupport Wizard'
+ADDONTITLE     = 'KODI Support'
 EXCLUDES       = [ADDON_ID]
 # Text File with build info in it.
-BUILDFILE      = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/buildsfile.txt'
+BUILDFILE      = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/builds.txt'
 # How often you would list it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK    = 0
 # Text File with apk info in it.
-APKFILE        = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/apksfile.txt'
+APKFILE        = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/apk.txt'
 # Text File with Youtube Videos urls.  Leave as 'http://' to ignore
 YOUTUBETITLE   = ''
 YOUTUBEFILE    = 'http://'
 # Text File for addon installer.  Leave as 'http://' to ignore
-ADDONFILE      = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/addonsinstallerfile.txt'
+ADDONFILE      = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/addon.txt'
 # Text File for advanced settings.  Leave as 'http://' to ignore
-ADVANCEDFILE   = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/advancedsettingsfile.txt'
+ADVANCEDFILE   = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/advanced.txt'
 
 # Dont need to edit just here for icons stored locally
 PATH           = xbmcaddon.Addon().getAddonInfo('path')
@@ -34,17 +34,17 @@ ART            = os.path.join(PATH, 'resources', 'art')
 # Example:  ICONMAINT     = os.path.join(ART, 'mainticon.png')
 #           ICONSETTINGS  = 'http://aftermathwizard.net/repo/wizard/settings.png'
 # Leave as http:// for default icon
-ICONBUILDS     = os.path.join(ART, 'iconbuilds.png')
-ICONMAINT      = os.path.join(ART, 'iconmaint.png')
-ICONAPK        = os.path.join(ART, 'iconapk.png')
-ICONADDONS     = os.path.join(ART, 'iconaddons.png')
-ICONYOUTUBE    = os.path.join(ART, 'iconyoutube.png')
-ICONSAVE       = os.path.join(ART, 'iconsave.png')
-ICONTRAKT      = os.path.join(ART, 'icontrakt.png')
-ICONREAL       = os.path.join(ART, 'iconreal.png')
-ICONLOGIN      = os.path.join(ART, 'iconlogin.png')
-ICONCONTACT    = os.path.join(ART, 'iconcontact.png')
-ICONSETTINGS   = os.path.join(ART, 'iconsettings.png')
+ICONBUILDS     = 'http://'
+ICONMAINT      = 'http://'
+ICONAPK        = 'http://'
+ICONADDONS     = 'http://'
+ICONYOUTUBE    = 'http://'
+ICONSAVE       = 'http://'
+ICONTRAKT      = 'http://'
+ICONREAL       = 'http://'
+ICONLOGIN      = 'http://'
+ICONCONTACT    = 'http://'
+ICONSETTINGS   = 'http://'
 # Hide the ====== seperators 'Yes' or 'No'
 HIDESPACERS    = 'No'
 # Character used in seperator
@@ -52,10 +52,10 @@ SPACER         = '*'
 
 # You can edit these however you want, just make sure that you have a %s in each of the
 # THEME's so it grabs the text from the menu item
-COLOR1         = 'dodgerblue'
+COLOR1         = 'deepskyblue'
 COLOR2         = 'white'
 # Primary menu items   / %s is the menu item and is required
-THEME1         = '[COLOR '+COLOR1+'][B][I]([COLOR '+COLOR2+']Kodi Support[/COLOR])[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR][/I]'
+THEME1         = '[COLOR '+COLOR1+'][B][I]([COLOR '+COLOR2+']Aftermath[/COLOR])[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR][/I]'
 # Build Names          / %s is the menu item and is required
 THEME2         = '[COLOR '+COLOR2+']%s[/COLOR]'
 # Alternate items      / %s is the menu item and is required
@@ -69,7 +69,7 @@ THEME5         = '[COLOR '+COLOR1+']Current Theme:[/COLOR] [COLOR '+COLOR2+']%s[
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
 HIDECONTACT    = 'No'
 # You can add \n to do line breaks
-CONTACT        = 'Thank you for choosing Kodi Support Wizard.\r\n\r\nContact us on facebook at https://www.facebook.com/groups/1845268792391503'
+CONTACT        = 'Thank you for choosing Kodi Support Wizard.\r\n\r\nContact us on facebook at https://www.facebook.com/groups/1845268792391503/'
 #Images used for the contact window.  http:// for default icon and fanart
 CONTACTICON    = 'http://'
 CONTACTFANART  = 'http://'
@@ -81,34 +81,34 @@ CONTACTFANART  = 'http://'
 # Enable Auto Update 'Yes' or 'No'
 AUTOUPDATE     = 'Yes'
 # Url to wizard version
-WIZARDFILE     = ''
+WIZARDFILE     = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/autobuilds.txt'
 #########################################################
 
 #########################################################
 ### AUTO INSTALL ########################################
 ########## REPO IF NOT INSTALLED ########################
 # Enable Auto Install 'Yes' or 'No'
-AUTOINSTALL    = 'No'
+AUTOINSTALL    = 'Yes'
 # Addon ID for the repository
-REPOID         = ''
+REPOID         = 'repository.wolfcreation'
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
-REPOADDONXML   = ''
+REPOADDONXML   = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/zips/addon.xml'
 # Url to folder zip is located in
-REPOZIPURL     = ''
+REPOZIPURL     = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/zips/'
 #########################################################
 
 #########################################################
 ### NOTIFICATION WINDOW##################################
 #########################################################
 # Enable Notification screen Yes or No
-ENABLE         = 'No'
+ENABLE         = 'Yes'
 # Url to notification file
-NOTIFICATION   = ''
+NOTIFICATION   = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/notify.txt'
 # Use either 'Text' or 'Image'
-HEADERTYPE     = ''
-HEADERMESSAGE  = ''
+HEADERTYPE     = 'Text'
+HEADERMESSAGE  = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/'
 # url to image if using Image 424x180
 HEADERIMAGE    = ''
 # Background for Notification Window
-BACKGROUND     = ''
+BACKGROUND     = 'https://raw.githubusercontent.com/wadeknelsen/wolfcreations/master/wizard/wizard.png'
 #########################################################
